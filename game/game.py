36b6c497dc28e1cc316a7d19cd322f5c4db91513
Arthur Lozano <chillout885@gmail.com>
2022-02-16 20:04:49 -0800

    created enemies.py and enabled it in main game.py

--- game/game.py
+++ game/game.py
@@ -1,20 +1,29 @@
 import pygame, sys
 from sprite import Sprite
+from enemies import Enemy
 
-YELLOW = (255,255,0)
+YELLOW = (255, 255, 0)
+BLUE = (0, 0, 255)
+
 
 class Game:
     def __init__(self):
         player_sprite = Sprite(YELLOW, 20, 20, (300, 600))
+        sprite_enemy = Enemy(BLUE, 30, 30, (200, 450))
         self.player = pygame.sprite.GroupSingle(player_sprite)
+        self.enemy = pygame.sprite.GroupSingle(sprite_enemy)
 
     def run(self):
         self.player.update()
         self.player.draw(screen)
 
-if __name__ == '__main__':
+    def run_enemy(self):
+        self.enemy.draw(screen)
+
+
+if __name__ == "__main__":
     pygame.init()
-    screen = pygame.display.set_mode((600,600))
+    screen = pygame.display.set_mode((600, 600))
     pygame.display.set_caption("Lazer Python")
     clock = pygame.time.Clock()
     game = Game()
@@ -28,8 +37,9 @@ if __name__ == '__main__':
                 sys.exit()
                 moving = False
 
-        screen.fill((10,10,10))
+        screen.fill((10, 10, 10))
         game.run()
+        game.run_enemy()
 
         pygame.display.flip()
 
