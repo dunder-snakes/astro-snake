598c31e9cf407433f9d0a64c3909665b86eb2c22
Minhang Xie <minxie97@gmail.com>
2022-02-19 12:42:05 -0800

    added constants to easily scale up resolution

--- game/enemies.py
+++ game/enemies.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import constants as c
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
@@ -11,7 +12,7 @@ class Enemy(pygame.sprite.Sprite):
         self.enemy = pygame.transform.scale(self.en_image,(30, 30))
         self.image.blit(self.enemy,(0,0))
         self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(0, 600-self.rect.width)
+        self.rect.x = random.randrange(0, c.DISPLAY_X-self.rect.width)
         self.rect.y = -self.rect.height
         self.speed = random.randrange(1, 5)
 
