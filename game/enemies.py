bd827a9db33cee24b6077cc2443b561a22a074ee
Minhang Xie <minxie97@gmail.com>
2022-02-19 12:09:23 -0800

    enemy spawn adjustments, clean up

--- game/enemies.py
+++ game/enemies.py
@@ -1,16 +1,19 @@
 import pygame
+import random
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self,height, width, pos):
+    def __init__(self):
         super().__init__()
 
-        self.image = pygame.Surface([width, height])
+        self.image = pygame.Surface([30, 30])
         ## Change picture to mongoose
-        self.en_image = pygame.image.load("/assets/snake.png")
-        self.enemy = pygame.transform.scale(self.en_image,(width, height))
+        self.en_image = pygame.image.load("../assets/snake.png").convert_alpha()
+        self.enemy = pygame.transform.scale(self.en_image,(30, 30))
         self.image.blit(self.enemy,(0,0))
-
-        self.rect = self.image.get_rect(midbottom=pos)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0, 600-self.rect.width)
+        self.rect.y = -self.rect.height
+        self.speed = random.randrange(1, 5)
 
     def update(self):
-        self.rect.y += 1
+        self.rect.y += self.speed
